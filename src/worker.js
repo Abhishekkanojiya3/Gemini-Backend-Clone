0063bedeedb01dda247e8a716944ddf44ddf2b55
Abhishek-cyno <dev.in3@cyno.co.in>
2025-07-17 15:10:45 +0530

    Port for background worker

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,12 @@
 import "./workers/geminiWorker.js";
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Worker is running (with dummy server).');
+});
+
+app.listen(PORT, () => {
+  console.log(`Dummy server running on port ${PORT}`);
+});
